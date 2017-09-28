3a8faa1c293f63e14238e3491de7784f4ad2a439
medmondson <eddy556@gmail.com>
2017-09-28 20:49:51 +0100

    Initial check-in and first test

---
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CheckoutKata
+{
+    public class Checkout:ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutKata/CheckoutKataTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckoutKataTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckoutKataTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5545bd37-b47b-40a8-a4af-8b7bc23c8fda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
